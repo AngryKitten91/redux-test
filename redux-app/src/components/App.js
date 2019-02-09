7260beee28ab0ffbbbc8ec1a8f4237ed3b2e644a
tomekkoszykowski <tomasz.k@shedul.com>
2019-02-09 09:13:19 +0100

    clicker

--- redux-app/src/components/App.js
+++ redux-app/src/components/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
+import Clicker from 'components/Clicker'
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        <div>App</div>
-        <div></div>
-      </div>
-    );
+    return <div>
+      <Clicker />
+    </div>
   }
 }
 
