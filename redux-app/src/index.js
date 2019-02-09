420f87d787f6fc3a1a75ef13e98e3163c904a083
tomekkoszykowski <tomasz.k@shedul.com>
2019-02-09 12:34:00 +0100

    redux dev tools

--- redux-app/src/index.js
+++ redux-app/src/index.js
@@ -22,7 +22,10 @@ function reducer(state = 10, action) {
   }
 }
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
