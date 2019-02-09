ead9e7cabb98b3788cfa083cd2d3560516f6a781
tomekkoszykowski <tomasz.k@shedul.com>
2019-02-09 09:04:04 +0100

    boilerplate

---
+++ redux-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>App</div>
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default App;
