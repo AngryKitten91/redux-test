e526593aa40505ddf4231ea2b6f76040a99cd41d
tomekkoszykowski <tomasz.k@shedul.com>
2019-02-09 10:48:38 +0100

    react redux connect dispatch props

--- redux-app/src/components/App.js
+++ redux-app/src/components/App.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux'
 
-// const actionInc = { type: 'INCREMENT' };
+const actionInc = { type: 'INCREMENT' };
 // const actionDec = { type: 'DECREMENT' };
 
 class App extends Component {
   render() {
-    const { reduxStore } = this.props;
+    const { reduxStore, increment } = this.props;
 
     return <div>
       <div>App</div>
       <div>{reduxStore}</div>
+      <div onClick={increment}>+</div>
     </div>
   }
 }
 
-const mapStateToProps = (state) => {
-  return {
-    reduxStore: state,
-  }
-}
+const mapStateToProps = (state) => ({
+  reduxStore: state,
+});
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = (dispatch) => ({
+  increment: () => {
+    dispatch(actionInc)
+  },
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
