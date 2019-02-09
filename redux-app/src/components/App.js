06635b3819bbf0f7e82e98e4fcd0c6117b69b97c
tomekkoszykowski <tomasz.k@shedul.com>
2019-02-09 10:12:13 +0100

    react redux init

--- redux-app/src/components/App.js
+++ redux-app/src/components/App.js
@@ -1,37 +1,7 @@
 import React, { Component } from 'react';
 
-import { createStore } from 'redux';
-
 const actionInc = { type: 'INCREMENT' };
 const actionDec = { type: 'DECREMENT' };
-
-function reducer(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch(actionInc);
-store.dispatch(actionInc);
-store.dispatch(actionInc);
-store.dispatch(actionInc);
-
-store.dispatch(actionDec);
-store.dispatch(actionDec);
-
-
-
 
 class App extends Component {
   render() {
